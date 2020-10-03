2164d6e8ae6b57749cb9f0001c2b3a3d187e1fa9
Faizaan Khan <faizaankhan24@gmail.com>
2020-10-03 14:32:36 +0530

    Initial check-in of module Plutus

---
+++ Plutus/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Plutus.Models;
+
+namespace Plutus.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
