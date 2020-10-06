924af0b1e6a31dd341c9185f5701e810a58617a9
Faizaan Khan <faizaankhan24@gmail.com>
2020-10-06 21:14:32 +0530

    Adding ResourcesDicitionary files for adding common styles and colors.
    Styles is a ResourceDictionary that is added to maintain all the sytles and colors that will be used in the app in a common place.
    
    Themes/LightTheme is added to maintain all the common styles so the app UI is consitent.

---
+++ Plutus/Views/Styles.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Plutus.Views
+{
+    public partial class Styles : ResourceDictionary
+    {
+        public Styles()
+        {
+            InitializeComponent();
+        }
+    }
+}
